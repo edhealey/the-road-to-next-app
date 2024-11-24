7fc178900ae5b44ff00fe738087cdfb06758ed54
edhealey <edhealey@earthlink.net>
2024-11-24 18:16:03 -0500

    changes through path constants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Link from 'next/link';
+import { ticketsPath } from '@/path';
+
 const HomePage = () => {
   return (
-    <h2>Home Page</h2>
+    <div>
+      <h2 className="text-lg">Home Page</h2>
+      <Link href={ticketsPath()} className="underline">
+        Go to Tickets
+      </Link>
+    </div>   
   );
 }
 
